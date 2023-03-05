bf3c824d405cbcd99956d3ccb068f2221cd6f9a8
Icey Python <samkellyndungu@gmail.com>
2023-03-05 09:24:17 +0300

    Added: logic to hyper the text

--- app.js
+++ app.js
@@ -1,4 +1,23 @@
 var text = document.querySelector('h2');
-text.onmouseover=()=>{
-    console.log(text.textContent)
+text.onmouseover=(e)=>{
+    //letters to iterate through
+    let letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let iteration = 0;
+    let transformationInterval = setInterval(()=>{
+        //split the text,cycle through the text and assign new letter for each
+        e.target.textContent = e.target.textContent.split("")
+        .map((letter,index) =>{
+            //if index is less than the iterations return the original
+            if(index < iteration){
+                return e.target.dataset.value[index];
+            }
+            return letters[Math.floor(Math.random() *26)]
+        })
+        .join("");
+        //set iteration to the length of the text
+        if(iteration >= e.target.dataset.value.length){
+            clearInterval(transformationInterval)
+        }
+        iteration+=1/3;
+    },30)
 }
